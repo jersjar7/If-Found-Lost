864b23e1130343e8c34463269a5eb1957585f7c9
Jerson <jerson.7@icloud.com>
2025-05-04 20:56:24 -0600

    Initial Commit

---
+++ android/app/src/main/kotlin/com/jersondevs/if_found_lost/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jersondevs.if_found_lost
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'If Found Lost',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('If Found Lost')),
+      body: Center(child: Text('QR Code Sticker System')),
+    );
+  }
+}
